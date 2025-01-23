d94c4a9309126ab39e2af0348321ffd2a85cbfec
Cian Hughes <chughes000@gmail.com>
2025-01-23 10:39:49 +0000

    Improved container entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,21 @@
 
 mkdir -p ~/.config/nvim
 
-if [ -z "$(ls -A ~/.config/nvim)" ]; then
+WORKDIR=$(pwd)
+
+cd /my_nvim_config
+git remote update
+if ! git rev-list --count HEAD..@{upstream} > /dev/null; then
+    git pull
+    STALE_CONFIG=true
+else
+    STALE_CONFIG=false
+fi
+
+if [ ! "$(ls -A ~/.config/nvim)" ] || $STALE_CONFIG; then
     cp -r /my_nvim_config/* ~/.config/nvim/
 fi
+
+cd "$WORKDIR"
+
 /usr/bin/env nvim
